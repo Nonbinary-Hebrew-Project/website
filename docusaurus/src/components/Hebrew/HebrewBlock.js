2d5ac8623b1801943c407881811b9ccd3056b0f3
Thena Seer <11550396+blueridger@users.noreply.github.com>
2024-09-23 11:52:55 -0700

    test hebrew block working

--- docusaurus/src/components/Hebrew/HebrewBlock.js
+++ docusaurus/src/components/Hebrew/HebrewBlock.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function HebrewBlock({children, color}) {
   return (
     <span
+      dir="rtl"
       style={{
         backgroundColor: color,
         borderRadius: '2px',
