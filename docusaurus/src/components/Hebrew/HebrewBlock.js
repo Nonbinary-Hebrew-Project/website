44503f2a4483f3181810082141a76945246b96be
Thena Seer <11550396+blueridger@users.noreply.github.com>
2025-01-30 13:33:31 -0800

    add transliteration to hebrew block

--- docusaurus/src/components/Hebrew/HebrewBlock.js
+++ docusaurus/src/components/Hebrew/HebrewBlock.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from "react";
+import { transliterate } from "hebrew-transliteration";
 
-export default function HebrewBlock({children}) {
+export default function HebrewBlock({ children }) {
+  const [showTrans, setShowTrans] = useState(false);
+  const handle = useCallback(() => setShowTrans(!showTrans), [showTrans]);
   return (
-    <span
-      dir="rtl"
-      style={{
-        backgroundColor: "lightgrey",
-        borderRadius: '2px',
-        color: 'black',
-        fontSize: '1.5rem',
-        padding: '0.2rem',
-      }}>
-      {children}
-    </span>
+    <>
+      <span
+        dir="rtl"
+        style={{
+          backgroundColor: "lightgrey",
+          borderRadius: "2px",
+          color: "black",
+          fontSize: "1.5rem",
+          padding: "0.2rem",
+          cursor: "pointer",
+        }}
+        onClick={handle}
+      >
+        {children}
+      </span>
+      {showTrans && <div>{transliterate(children)}</div>}
+    </>
   );
 }
