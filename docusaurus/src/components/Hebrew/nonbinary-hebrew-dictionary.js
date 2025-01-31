8a77046916e373470f58a5856baf992ad6a86b10
Thena Seer <11550396+blueridger@users.noreply.github.com>
2025-01-30 22:29:06 -0800

    change dict to json with cms

--- docusaurus/src/components/Hebrew/nonbinary-hebrew-dictionary.js
+++ docusaurus/src/components/Hebrew/nonbinary-hebrew-dictionary.js
@@ -1,28 +1,4 @@
-var nonbinaryHebrewDictionary = [
-  {
-    lemma: "",
-    def: "",
-  },
-  {
-    lemma: "בְּרוּךֶ",
-    def: "Blessed (nonbinary)",
-  },
-  {
-    lemma: "ײַ",
-    def: "G-d",
-  },
-  {
-    lemma: "בָּרוּךְ",
-    def: "Blessed",
-  },
-  {
-    lemma: "הָעוֹלָם",
-    def: "of the world",
-  },
-  {
-    lemma: "ה'",
-    def: "G-d",
-  },
-];
+var json = require("./nonbinary-hebrew-dictionary.json");
+var nonbinaryHebrewDictionary = json.translations;
 
 module.exports = nonbinaryHebrewDictionary;
