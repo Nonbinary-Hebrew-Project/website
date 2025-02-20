34fe9d1d8f1b83968feb1ffcf3ce69d78b0e7d6f
Thena Seer <11550396+blueridger@users.noreply.github.com>
2025-02-20 15:45:30 -0800

    PdfViewer

---
+++ docusaurus/src/components/PdfViewers.js
@@ -0,0 +1,10 @@
+export default function PdfViewer({ frontMatter }) {
+  if (!frontMatter?.pdf_file) return null;
+  return (
+    <iframe
+      src={`https://docs.google.com/gview?url=https://nonbinary-hebrew-project.netlify.app/${frontMatter.pdf_file}&embedded=true`}
+      style="width:100%; height:30rem;"
+      frameborder="0"
+    ></iframe>
+  );
+}
--- docusaurus/src/theme/MDXComponents.js
+++ docusaurus/src/theme/MDXComponents.js
@@ -2,10 +2,12 @@ import React from "react";
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents";
 import HebrewBlock from "@site/src/components/Hebrew/HebrewBlock";
+import PdfViewer from "../components/PdfViewers";
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   HebrewBlock,
   HB: HebrewBlock,
+  PdfViewer,
 };
