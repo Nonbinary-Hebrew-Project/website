2b9f956dfbd80951deba5122c41240c0e380cca8
Thena Seer <11550396+blueridger@users.noreply.github.com>
2025-03-02 11:40:22 -0800

    improve contact form styling

--- docusaurus/src/components/ContactForm.js
+++ docusaurus/src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 export default function ContactForm() {
   const [result, setResult] = React.useState("");
 
@@ -11,7 +11,7 @@ export default function ContactForm() {
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      body: formData
+      body: formData,
     });
 
     const data = await response.json();
@@ -28,21 +28,30 @@ export default function ContactForm() {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <div style={{display:"flex", flexDirection: "column"}}>
+        <div style={{ display: "flex", flexDirection: "column", marginBottom: "3rem" }}>
+          <label htmlFor="name-input" style={{fontWeight:"bold"}}>Name:</label>
+          <input id="name-input" type="text" name="name" required />
 
-        Name:
-        <input type="text" name="name" required/>
-        Email:
-        <input type="email" name="email" required/>
-        Message:
-        <textarea name="message" required></textarea>
+          <label htmlFor="email-input" style={{fontWeight:"bold", paddingTop:"0.5rem"}}>Email address:</label>
+          <input id="email-input" type="email" name="email" required />
 
-        <button type="submit">Submit Form</button>
+          <label htmlFor="message-input" style={{fontWeight:"bold", paddingTop:"0.5rem"}}>Message:</label>
+          <textarea
+            id="message-input"
+            name="message"
+            required
+            rows="4"
+            style={{ resize: "vertical" }}
+          ></textarea>
+          <button
+            type="submit"
+            style={{ marginTop: "1rem", padding: "0.5rem", cursor: "pointer" }}
+          >
+            Submit Form
+          </button>
         </div>
-
       </form>
       <span>{result}</span>
-
     </div>
   );
 }
