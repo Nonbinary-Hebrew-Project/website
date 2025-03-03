5185720d1c86d16968eac08456464129ba4f1142
Thena Seer <11550396+blueridger@users.noreply.github.com>
2025-03-02 18:27:15 -0800

    improve transliteration tool styling

--- docusaurus/src/pages/transliterate.tsx
+++ docusaurus/src/pages/transliterate.tsx
@@ -12,10 +12,28 @@ export default function TransliteratePage() {
       title={`${siteConfig.title}`}
       description="We are building a bigger tent for nonbinary Jews through a third-gender grammar systematics for Hebrew, guided by our Torah and Talmud that teach us to rejoice that which cannot be neatly categorized."
     >
-      <div style={{display:"flex", flexDirection:"column", alignItems:"center", margin:"2rem"}}>
-      <h1>Transliteration Tool</h1>
-      <textarea style={{width:"50%", resize:"vertical"}} rows={4} onChange={(e) => setValue(e.target.value)}></textarea>
-      {value ? <HebrewBlock>{value}</HebrewBlock> : <p>Type or paste hebrew in the box above and it will be transliterated here.</p>}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "2rem",
+        }}
+      >
+        <h1>Transliteration Tool</h1>
+        <textarea
+          style={{ direction:"rtl", maxWidth:"100%", width:"40rem", resize: "vertical" }}
+          rows={5}
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
+        {value ? (
+          <HebrewBlock>{value}</HebrewBlock>
+        ) : (
+          <p>
+            Type or paste hebrew in the box above and it will be transliterated
+            here.
+          </p>
+        )}
       </div>
     </Layout>
   );
