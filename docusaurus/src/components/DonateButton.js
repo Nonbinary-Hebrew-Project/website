6a72557671b2397b6285744dc3b82ffeee79acdb
Thena Seer <11550396+blueridger@users.noreply.github.com>
2026-01-26 14:51:42 -0800

    Donate button minor

--- docusaurus/src/components/DonateButton.js
+++ docusaurus/src/components/DonateButton.js
@@ -1,8 +1,8 @@
 export default function DonateButton() {
   return <iframe
-        src="https://donate.mazloweb.com/donate/CRcBKhLb8raLJVm3AstCV1"
-        height="480"
-        width="450"
+        src="https://donate.mazloweb.com/donate/blossom-with-the-nonbinary-hebrew-project"
+        height="850"
+        width="500"
         style={{border: "none"}}
         sandbox="allow-forms allow-scripts allow-top-navigation allow-same-origin allow-popups allow-popups-to-escape-sandbox"
       ></iframe>
